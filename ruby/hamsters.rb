db7d5c1f8eeb15ea83f9873487c6b77ecd146acb
Wauriman Borges <waurimanpilot@gmail.com>
2016-12-11 09:24:24 -0800

    challenge 4.3 mandatory

---
+++ ruby/hamsters.rb
@@ -0,0 +1,16 @@
+puts "Input hamsters name:"
+hamsters_name = gets.to_s
+puts "On scale of 1 to 10, how loud is the  hamster?"
+hamsters_loud = gets.to_i
+puts "what color is your hamster?"
+hamsters_fur = gets.to_s
+puts "is the hamster good for adoption?"
+hamsters_adoption = gets.to_s
+puts "what is the age of the hamster?"
+hamsters_age = gets.chomp
+if hamsters_age.empty?
+	hamsters_age = nil
+else 
+	hamster_age = hamsters_age.to_i
+end
+puts "The name of your Hamster is #{hamsters_name}, he squeeks at the volume of #{hamsters_loud},  this hamsters fur is #{hamsters_fur}, is the hamster good for adoption? #{hamsters_adoption}, the hamsters age is #{hamsters_age}"

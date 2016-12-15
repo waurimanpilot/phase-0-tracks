9d078da49709888b33ad4ebf1bdf527a5b34ca4f
Wauriman Borges <waurimanpilot@gmail.com>
2016-12-15 09:20:44 -0800

    finished documentation

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".<upcase>
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".insert(-1, ' suspects')
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
-
-# "How many times does the letter 'a' appear in this string?".<???>
+# number 122 is the return integer ordinal of one-character string "z"
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4

7ae538ee0ea96d8f13973edbbcda68d28c1b8bed
Wauriman Borges <waurimanpilot@yahoo.com>
2017-03-13 00:46:21 -0700

    4.4 vampires file

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,6 +4,7 @@ puts "what is your name?"
 name_you = gets.chomp
 puts "how old are you?"
 age_you = gets.chomp
+#gettting input from the user and storing
 puts "what year were you born?"
 year_you = gets.chomp
 puts "would you like some garlic bread?"
@@ -12,10 +13,11 @@ puts "would you enroll in the company's h.i.?"
 health_you = gets.chomp
 puts "please name any allergies:"
 aller_you = gets.chomp
-
+#printing the results of the user input
 p "#{emp_proc}, #{name_you}, #{age_you}, #{year_you}, #{garlic_you}, #{health_you}, #{aller_you}"
+#calculating the user age to find if he has the right age by looping
 
-age_right = (2016 - year_you.to_i)
+age_right = (2017 - year_you.to_i)
 if age_right.empty?
 	age_right = nil
 	else

26bef56f9f127df2de07583413fa2223764a4f94
Wauriman Borges <waurimanpilot@yahoo.com>
2017-03-20 00:20:34 -0700

    5.4 nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,25 @@
 #design a nested structure of classroom > desks >supplies
 #print few individual last layer data, show indexes || hash keys || both to acess nested items.
 
-classroom = {
-	desk1: ['papers', 'pencils', 'erasers'],
-	desk2: ['books' , 'notepads'],
-	desk3: ['glues', 'paints', 'colored pens'],
-	desk4: ['scissors', 'staplers', 'toners'],
-	desk5: []
+#creating a nested data structure with classroom items
+  classroom = {
+	desk1: ['Papers', 'Pencils', 'Erasers'
+	],
+	desk2: {'books'=> 'math', 'notepads'=> "sketch", 'pads'=> 'eletronic'
+	  
+	},
+	desk3: ['glues', 'paints', 'colored pens'
+	],
+	desk4: ['scissors', 'staplers', 'toners'
+	],
+	desk5: [
+	  ]
 }
 #access nested desks
 p classroom
-		classroom[:desk1][2]
-		classroom[:desk3][0]
+		classroom[:desk1].reverse
+		classroom[:desk3][1].capitalize
 		classroom[:desk5].push('computers')
 		classroom[:desk1][2] = 'pens'
 p classroom
+ 
